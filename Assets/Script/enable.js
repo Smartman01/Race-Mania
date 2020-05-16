c20c2c3ff2eaf69b5f5ea3fd76d41e3ba592f168
Carl Antoine <carlantoine14@gmail.com>
2020-05-16 19:35:39 -0400

    First Game I made

---
+++ Assets/Script/enable.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+var NextScene : GameObject;
+var Fireworks : GameObject;
+
+function OnTriggerEnter(other : Collider) {
+	NextScene.SetActive(true);
+	Fireworks.SetActive(true);
+}
---
+++ Assets/Script/playButton.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+var LoadingImage : GameObject;
+
+function LoadScene (level : int) {
+		LoadingImage.SetActive(true);
+		Application.LoadLevel (level);
+}
